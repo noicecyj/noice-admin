7bd9ea8d5f4c7555ce2de3e9d3e9b2c071172cf2
noice <952637212@qq.com>
2022-03-16 22:15:10 +0800

    开发计划

--- cyj-entity-creater/src/main/java/com/example/cyjentitycreater/service/custom/Impl/EntityNameCustomServiceImpl.java
+++ cyj-entity-creater/src/main/java/com/example/cyjentitycreater/service/custom/Impl/EntityNameCustomServiceImpl.java
@@ -453,8 +453,8 @@ public class EntityNameCustomServiceImpl extends BaseService implements EntityNa
         sb.append("\r\n");
         sb.append("import com.example.cyjcommon.service.BaseService;\r\n");
         sb.append("import ").append(poDaoPath).append(poName).append("Dao;\r\n");
-        sb.append("import ").append(poPath).append(poName).append("PO;\r\n");
         sb.append("import ").append(poPath).append("Q").append(poName).append("PO;\r\n");
+        sb.append("import ").append(poPath).append(poName).append("PO;\r\n");
         sb.append("import ").append(poServicePath).append(poName).append("Service;\r\n");
         sb.append("import com.querydsl.core.QueryResults;\r\n");
         sb.append("import org.springframework.beans.factory.annotation.Autowired;\r\n");
@@ -526,8 +526,8 @@ public class EntityNameCustomServiceImpl extends BaseService implements EntityNa
         sb.append("package ").append(poServicePath).append("Impl;\r\n");
         sb.append("\r\n");
         sb.append("import ").append(poDaoPath).append(poName).append("Dao;\r\n");
-        sb.append("import ").append(poPath).append(poName).append("PO;\r\n");
         sb.append("import ").append(poPath).append("Q").append(poName).append("PO;\r\n");
+        sb.append("import ").append(poPath).append(poName).append("PO;\r\n");
         sb.append("import com.example.cyjcommon.service.BaseService;\r\n");
         sb.append("import ").append(poServicePath).append(poName).append("Service;\r\n");
         sb.append("import com.querydsl.core.QueryResults;\r\n");
