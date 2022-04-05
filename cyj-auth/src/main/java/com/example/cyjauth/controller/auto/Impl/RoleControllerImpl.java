c4bd0d39d8abfd4531e5eda4d050df8a2752ddcd
noice <952637212@qq.com>
2022-04-05 22:00:38 +0800

    开发计划

--- cyj-auth/src/main/java/com/example/cyjauth/controller/auto/Impl/RoleControllerImpl.java
+++ cyj-auth/src/main/java/com/example/cyjauth/controller/auto/Impl/RoleControllerImpl.java
@@ -1,9 +1,9 @@
 package com.example.cyjauth.controller.auto.Impl;
 
+import com.example.cyjcommon.utils.ResultVO;
 import com.example.cyjauth.controller.auto.RoleController;
 import com.example.cyjauth.entity.auto.po.RolePO;
 import com.example.cyjauth.service.auto.RoleService;
-import com.example.cyjcommon.utils.ResultVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
