5145299097a36e6e98c28e08c20d3f3a06f184af
noice <952637212@qq.com>
2022-04-10 18:47:29 +0800

    开发计划

---
+++ cyj-workflow/target/generated-sources/java/com/example/cyjworkflow/entity/auto/po/QBpmnPO.java
@@ -0,0 +1,43 @@
+package com.example.cyjworkflow.entity.auto.po;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QBpmnPO is a Querydsl query type for BpmnPO
+ */
+@Generated("com.querydsl.codegen.EntitySerializer")
+public class QBpmnPO extends EntityPathBase<BpmnPO> {
+
+    private static final long serialVersionUID = -167769443L;
+
+    public static final QBpmnPO bpmnPO = new QBpmnPO("bpmnPO");
+
+    public final StringPath id = createString("id");
+
+    public final StringPath processName = createString("processName");
+
+    public final StringPath sortCode = createString("sortCode");
+
+    public final StringPath status = createString("status");
+
+    public QBpmnPO(String variable) {
+        super(BpmnPO.class, forVariable(variable));
+    }
+
+    public QBpmnPO(Path<? extends BpmnPO> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QBpmnPO(PathMetadata metadata) {
+        super(BpmnPO.class, metadata);
+    }
+
+}
+
