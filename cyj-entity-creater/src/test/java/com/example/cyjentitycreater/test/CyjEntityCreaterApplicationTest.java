a8e41ab32e384e05a61c7d46268d6394b85cb8f3
noice <952637212@qq.com>
2022-05-26 20:54:49 +0800

    开发计划

--- cyj-entity-creater/src/test/java/com/example/cyjentitycreater/test/CyjEntityCreaterApplicationTest.java
+++ cyj-entity-creater/src/test/java/com/example/cyjentitycreater/test/CyjEntityCreaterApplicationTest.java
@@ -8,42 +8,34 @@ import lombok.Data;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MockMvcBuilder;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
-import org.springframework.web.servlet.mvc.condition.RequestConditionHolder;
 
 /**
  * @author Noice
  */
 @Data
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = CyjEntityCreaterApplication.class,webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(classes = CyjEntityCreaterApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class CyjEntityCreaterApplicationTest {
 
     @Autowired
     public WebApplicationContext wac;
     private MockMvc mockMvc;
 
-    public void setup(){
+    public void setup() {
         this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
         MockHttpServletRequest request = new MockHttpServletRequest();
         RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request));
     }
+
     @Autowired
     private EntityCustomService entityCustomService;
     @Autowired
@@ -51,10 +43,9 @@ public class CyjEntityCreaterApplicationTest {
 
     @Test
     @Transactional
-    public void entityTest(){
-        EntityPO entityPO = entityDao.getOne("1b6e7b0d-8758-4de0-a80f-b54505ac9e9d");
+    public void entityTest() {
+        EntityPO entityPO = entityDao.getOne("1b6e7b0d-8758-4de0-a80f-b54505ac9e91");
         entityCustomService.entityHandler(entityPO);
-
     }
 
 }
