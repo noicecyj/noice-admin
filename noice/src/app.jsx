4b53aaa549552c1aefedf6481ca3ab0e7fcc590d
noice <952637212@qq.com>
2022-06-10 20:12:20 +0800

    开发计划

--- noice/src/app.jsx
+++ noice/src/app.jsx
@@ -14,7 +14,6 @@ const appConfig = {
       request: {
         onConfig: (config) => {
           // 发送请求前：可以对 RequestConfig 做一些统一处理
-          console.log("config---->",config);
           if (config.url !== '/authApi/v1/login') {
             // eslint-disable-next-line no-param-reassign
             config.headers = {Authorization: `Bearer ${cookie.load('token')}`};
